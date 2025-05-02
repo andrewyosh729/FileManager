8e26c9ea0ea459f7172683ec6c56d779f2c97d9e
Yoshi <andrewyosh@yahoo.com>
2025-05-02 16:04:20 -0500

    Move files around

--- FileManager/Collections/FileSystemInfoChildren.cs
+++ FileManager/Collections/FileSystemInfoChildren.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using Avalonia.Media.TextFormatting.Unicode;
@@ -54,37 +53,5 @@ public class FileSystemInfoChildren : ConcurrentObservableList<FileSystemInfoWra
         }
 
         return 0;
-    }
-}
-
-public class ThrottledTaskQueue
-{
-    private Task? MainTask { get; set; }
-    private bool HasContinuationQueued { get; set; }
-    private object LockObject { get; } = new();
-
-    public void QueueWork(Func<Task> work)
-    {
-        lock (LockObject)
-        {
-            if (MainTask == null || MainTask.IsCompleted)
-            {
-                // Run immediately
-                MainTask = work();
-                HasContinuationQueued = false;
-            }
-            else if (!HasContinuationQueued)
-            {
-                // Queue continuation only once
-                HasContinuationQueued = true;
-                MainTask = MainTask.ContinueWith(_ => { return work(); }).Unwrap().ContinueWith(_ =>
-                {
-                    lock (LockObject)
-                    {
-                        HasContinuationQueued = false;
-                    }
-                });
-            }
-        }
     }
 }
---
+++ FileManager/Utils/ThrottledTaskQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FileManager.Collections;
+
+public class ThrottledTaskQueue
+{
+    private Task? MainTask { get; set; }
+    private bool HasContinuationQueued { get; set; }
+    private object LockObject { get; } = new();
+
+    public void QueueWork(Func<Task> work)
+    {
+        lock (LockObject)
+        {
+            if (MainTask == null || MainTask.IsCompleted)
+            {
+                // Run immediately
+                MainTask = work();
+                HasContinuationQueued = false;
+            }
+            else if (!HasContinuationQueued)
+            {
+                // Queue continuation only once
+                HasContinuationQueued = true;
+                MainTask = MainTask.ContinueWith(_ => work()).Unwrap().ContinueWith(_ =>
+                {
+                    lock (LockObject)
+                    {
+                        HasContinuationQueued = false;
+                    }
+                });
+            }
+        }
+    }
+}
